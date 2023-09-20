9ab3751842ab85a1c5fe91974d304dfb84b14b1e
Kopium-Net <87044411+officialmeritinfos@users.noreply.github.com>
2023-09-20 10:32:09 +0100

    Update constant.php
    Updated keys

--- config/constant.php
+++ config/constant.php
@@ -3,7 +3,7 @@
 return [
     'tatum'        =>[
         'testnet_api'   =>'778f9c70-9d63-4da1-a01c-bcf747c3e750',
-        'livenet_api'   =>'0a132c37-fecd-4c52-9cb7-c20104e3b1b4',
+        'livenet_api'   =>'xxxxxxxxxxxxxxxxxxxxxx',
         'is_live'       =>1,
         'live_url'      =>'https://api-eu1.tatum.io/'
     ],
